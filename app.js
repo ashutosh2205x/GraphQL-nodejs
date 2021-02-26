aa83a8344b936b87d519e1a5e92dea3802219124
Ashutosh Bharti <ashutosh@headerlabs.com>
2021-02-26 05:34:55 +0530

    query runnable

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
-const express= require('express');
-const bodyparser = require('body-parser');
-
-
+const express = require("express");
+const bodyparser = require("body-parser");
+const { buildSchema } = require("graphql");
+const { graphqlHTTP } = require('express-graphql');
 const app = express();
 
-app.use(bodyparser.json( ))
+app.use(bodyparser.json());
 
-app.get('/', (req, res, next)=>{
-    res.send('Hello ')
-})
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+    type RootQuery {
+        events : [String!]!
+    }
 
+    type RootMutation {
+        createEvent(name: String) : String
+    }
+    schema {
+        query : RootQuery
+        mutation : RootMutation
+    }
+    `),
+    rootValue: {
+      //all resolvers
+      events: () => {
+        return ["ARRAY", "of", "Strings"];
+      },
+      createEvent : (args)=>{
+        return args, args.name
+      }
+    },
+    graphiql : true
+  })
+);
 
-app.listen(3000,()=>{
-console.log('started')
-})
+app.listen(3000, () => {
+  console.log("started");
+});
